6617d8b915300f460d37b2ac3b7f62edcdc7ebff
wwjd12 <curtvance.dev@hotmail.com>
2026-05-28 17:19:36 -0400

    update

---
+++ src/types/obd2Code.ts
@@ -0,0 +1,5 @@
+export interface OBD2Code {
+  code: string;
+  description: string;
+  likelyCauses: string[];
+}
---
+++ src/types/specCard.ts
@@ -0,0 +1,8 @@
+export interface SpecCard {
+  engine: string;
+  oilCapacity: string;
+  coolantCapacity: string;
+  oilType: string;
+  oilFilter: string;
+  airFilter: string;
+}
